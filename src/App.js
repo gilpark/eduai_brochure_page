f78370c2d6046cd6323bd822e8b97c9e7342b0f8
Gil Park <sendtogil@gmail.com>
2018-11-26 21:24:41 -0600

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              <li>
+                <a href={'/'}>EduAi</a>
+              </li>
+              <li>
+                <a href={'/app'}>AppAi</a>
+              </li>
+            </ul>
+          </header>
+          <div className={'content'}>
+            <Route path={'/'} exact={true} render={()=><div><h1>EduAi</h1></div>}/>
+            <Route path={'/app'} exact={true} render={()=><div><h1>AppAi</h1></div>}/>
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
-
 export default App;
